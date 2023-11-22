5a3d7a1865056fabd4ea31a3a335417f2fb7226e
Oleh-Synytskyi <skvartsovoleg@gmail.com>
2023-11-22 23:46:33 +0200

    Pathfinding

---
+++ Path.py
@@ -0,0 +1,110 @@
+import requests
+import os
+import csv
+import cv2
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
+from scipy import ndimage as nd
+from skimage.filters import sobel
+import pathfinding
+import pygame, sys
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder as asf
+from pathfinding.core.diagonal_movement import DiagonalMovement as DM
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+class Pathfinder:
+    # Setup
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.grid = Grid(matrix=matrix)
+        self.select_surf = pygame.image.load('Pictures 3x3/selection.png').convert_alpha()
+
+        # pathfinding
+        self.path = []
+
+    def draw_active_cell(self):
+        mouse_pos = pygame.mouse.get_pos()
+        row = mouse_pos[1] // 32
+        col = mouse_pos[0] // 32
+        current_cell_value = self.matrix[row][col]
+        if current_cell_value == 1:
+            rect = pygame.Rect((col * 32, row * 32), (32, 32))
+            screen.blit(self.select_surf, rect)
+
+    def create_path(self):
+# start
+       start_x, start_y = [1, 1]
+       start = self.grid.node(start_x, start_y)
+
+# end
+       mouse_position = pygame.mouse.get_pos()
+       end_x, end_y = mouse_position[0] // 32, mouse_position[1] // 32
+       end = self.grid.node(end_x, end_y)
+# path
+       finder = asf(diagonal_movement=DM.always)
+       self.path,_ = finder.find_path(start, end, self.grid)
+       self.grid.cleanup()
+
+    def draw_path(self):
+        if self.path:
+            points = []
+            for point in self.path:
+                x = (point[0] * 32) + 16
+                y = (point[1] * 32) + 16
+                points.append((x, y))
+                pygame.draw.circle(screen, '#4a4a4a', (x,y), 20)
+            pygame.draw.lines(screen, '#4a4a4a', False, points, 5)
+
+    def update(self):
+        self.draw_active_cell()
+        self.draw_path()
+
+# Pygame Setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 736))
+clock = pygame.time.Clock()
+
+# Game start
+bg_surf = pygame.image.load('Pictures 3x3/mines.png').convert()
+matrix = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+pfinder = Pathfinder(matrix)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pfinder.create_path()
+    screen.blit(bg_surf, (0, 0))
+    pfinder.update()
+
+    pygame.display.update()
+    clock.tick(60)
